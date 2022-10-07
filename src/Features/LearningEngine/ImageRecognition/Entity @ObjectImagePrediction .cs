0ceb42908cbefd0fc6c49062515a3dae608c96c7
llllillliiilllllllli <80502514+llllillliiilllllllli@users.noreply.github.com>
2022-10-07 15:00:41 +0700

    Create Entity @ObjectImagePrediction .cs

---
+++ src/Features/LearningEngine/ImageRecognition/Entity @ObjectImagePrediction .cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Reflection;
+
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Vision;
+using Microsoft.Data.Analysis;
+using Microsoft.ML.TensorFlow;
+
+namespace DxMLEngine.Features.ObjectDetection
+{
+    public class ObjectImagePrediction 
+    {
+        [ColumnName("PredictedLabel")]        
+        public string PredictedCategory;
+
+        [ColumnName("Score")]
+        public float[] Score;
+    } 
+}
