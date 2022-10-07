712bdaf054b34614a5b129255c8589c65c355d00
llllillliiilllllllli <80502514+llllillliiilllllllli@users.noreply.github.com>
2022-10-07 13:22:43 +0700

    Create Entity @ProductImage

---
+++ src/Features/LearningEngine/ImageRecognition/Entity @ProductInspection .cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using System.Data;
+
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.Data.Analysis;
+
+using DxMLEngine.Attributes;
+using DxMLEngine.Utilities;
+
+namespace DxMlEngine.Features.ProductInspection 
+{
+	public class ProductImage
+	{
+		[ColumnName("Image")]
+		public bytes[]? Image { set; get; }
+		
+		[ColumnName("ImagePath")]
+		public string? ImagePath { set; get; }
+		
+		[ColumnName("Category")]
+		public string? Category { set; get; }	
+	}
+}
