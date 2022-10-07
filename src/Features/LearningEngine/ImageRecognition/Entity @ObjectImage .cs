3ae82e16ac744ac0d2cd5f46f8a9b7c963f09264
llllillliiilllllllli <80502514+llllillliiilllllllli@users.noreply.github.com>
2022-10-07 14:59:47 +0700

    Create Entity @ObjectImage .cs

---
+++ src/Features/LearningEngine/ImageRecognition/Entity @ObjectImage .cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Reflection;
+
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Vision;
+using Microsoft.Data.Analysis;
+using Microsoft.ML.TensorFlow;
+
+namespace DxMLEngine.Features.ObjectDetection
+{
+    public class ObjectImage
+    {
+        [LoadColumn(0), ColumnName("ImagePath")]
+        public string ImagePath;
+
+        [LoadColumn(1), ColumnName("Label")]
+        public string Category;
+    }    
+}
