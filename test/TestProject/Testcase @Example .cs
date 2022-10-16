875f6058831a087424d7384f2eacb8cb67cad618
dusthink <nguyenquocdung4869@gmail.com>
2022-10-16 13:49:53 +0700

    Add test project to solution and project workspace

---
+++ test/TestProject/Testcase @Example .cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+        }
+    }
+}
